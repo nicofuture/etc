b02070fd6e45e03b920f9f0d54e79ab5017cab6c
Nicolas Sanchez <nicolas.sanchez@mac.com>
2019-05-21 16:38:04 -0400

    1

--- 3dmodel.js
+++ 3dmodel.js
@@ -286,6 +286,6 @@ let el = document.querySelector(".canvas");
 let viewer = new Viewer(el);
 
 viewer
-  .load("https://seekingmaya.github.io/etc-content/assets/model.glb")
+  .load("https://d1w166w3qr099y.cloudfront.net/YEEZY3D")
   .catch(e => console.log(e))
   .then(gltf => console.log("Done ", gltf));
